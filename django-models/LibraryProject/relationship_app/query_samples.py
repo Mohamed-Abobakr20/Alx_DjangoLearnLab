dacf38299cd3c5989f5b7857ca311384c84e7950
Mohamed Abobakr <abobakrmohamed2222@gmail.com>
2025-07-16 02:00:38 +0300

    updating code in query file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,17 @@
 from .models import Author, Book, Library, Librarian
 
-# Query 1: All books by a specific author
-author = Author.objects.get(name="J.K. Rowling")
+# Query 1: Books by a specific author
+author_name = "J.K. Rowling"
+author = Author.objects.get(name=author_name)
 books_by_author = author.books.all()
 print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
 
-# Query 2: List all books in a library
-library = Library.objects.get(name="Central Library")
+# Query 2: List all books in a specific library
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 print(f"Books in {library.name}: {[book.title for book in books_in_library]}")
 
-# Query 3: Retrieve the librarian for a library
+# Query 3: Get the librarian of the library
 librarian = library.librarian
 print(f"Librarian of {library.name}: {librarian.name}")
